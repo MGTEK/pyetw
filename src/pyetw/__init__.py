df24fbbc7cc3fad3803d9149a2a535d15fc4eeed
Marius Greuel <greuelm@mgtek.com>
2022-12-17 12:12:55 +0100

    Move metadata to pyproject.toml

---
+++ src/pyetw/__init__.py
@@ -0,0 +1,11 @@
+"""Python logging via Event Tracing for Windows."""
+
+#
+# Copyright (C) 2020 Marius Greuel
+#
+# SPDX-License-Identifier: MIT
+#
+# https://github.com/mgtek/pyetw
+#
+
+from .pyetw import Event, EventProvider, LoggerHandler
